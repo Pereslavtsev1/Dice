8436814d2c11d5f21209e035f3458c1cc5b3339c
Pereslavtsev Vladimir <vovapereslavtsev@gmail.com>
2024-12-07 18:17:29 +0300

    Added header and animation has been changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Dice from "./components/Dice";
+import Header from "./components/Header";
 import RollButton from "./components/RollButton";
 
 function App() {
@@ -8,21 +9,26 @@ function App() {
   const rollDice = () => {
     const newFaces = faces.map(() => Math.floor(Math.random() * 6) + 1);
     setFaces(newFaces);
+
     const sum = newFaces.reduce((partialSum, a) => partialSum + a, 0);
     console.log(sum); // 6
   };
-  // #dad3c1,#EFEAD0,#323437
+
   return (
-    <div className="flex items-center justify-center h-screen bg-[#323437]">
-      <div className="place-items-center w-[700px] p-5 flex-col bg-gray-200 rounded-2xl  shadow-lg">
+    <main className="flex flex-col items-center h-screen bg-[#323437]">
+      <Header />
+      <div className="flex mt-[10%] flex-col items-center w-[700px] p-5 bg-gray-200 rounded-2xl shadow-lg ">
         <div className="flex flex-row justify-center">
           {faces.map((face, index) => (
             <Dice key={index} face={face} />
           ))}
         </div>
-        <RollButton onRoll={rollDice} />
+
+        <div className="mt-5">
+          <RollButton onRoll={rollDice} />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { FaDice } from "react-icons/fa";
+const Header = () => {
+  return (
+    <>
+      <haeder className="w-dvw flex  m-5">
+        <FaDice className="mx-10 size-20 fill-[#d1d0c5]" />
+        <h1 className="text-[#d1d0c5]  text-7xl font-extrabold">Dice Game</h1>
+      </haeder>
+    </>
+  );
+};
+
+export default Header;
